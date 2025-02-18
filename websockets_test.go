50821b314dfd93c59df541c47099113ae556f037
Antkky <antiky222@gmail.com>
2025-02-18 08:03:44 -0800

    websockets test
    started on websockets tests

--- websockets_test.go
+++ websockets_test.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"testing"
+
+	"github.com/Antkky/go_crypto_scraper/structs"
+)
+
+var RouteSubscribeTestCases = []struct {
+	name           string
+	exchangeConfig structs.ExchangeConfig
+	errorValue     error
+	wantError      bool
+}{
+	// Test Cases
+	{},
+}
+
+func TestRouteSubscribe(t *testing.T) {
+
+}
